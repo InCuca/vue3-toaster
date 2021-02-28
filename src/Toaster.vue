f9a3c95f7bd33fc67d36c3f017446d1acc8b7f97
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-02-28 18:39:50 -0300

    feat: 🎸 Component operating logic

---
+++ src/Toaster.vue
@@ -0,0 +1,253 @@
+<template>
+  <transition
+    :enter-active-class="transition.enter"
+    :leave-active-class="transition.leave"
+  >
+    <div
+      v-show="isActive"
+      ref="toaster"
+      :class="['toast', `toast-${type}`, `toast-${position}`]"
+      @mouseover="toggleTimer(true)"
+      @mouseleave="toggleTimer(false)"
+      @click="click"
+      v-html="message"
+    />
+  </transition>
+</template>
+
+<script>
+import {
+  computed,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  reactive,
+  ref
+} from "vue";
+import useTimer from "./composables/timer";
+import useComponent from "./composables/component";
+import eventBus from "./composables/event-bus";
+
+export const POSITIONS = Object.freeze({
+  TOP_RIGHT: "top-right",
+  TOP: "top",
+  TOP_LEFT: "top-left",
+  BOTTOM_RIGHT: "bottom-right",
+  BOTTOM: "bottom",
+  BOTTOM_LEFT: "bottom-left"
+});
+
+export function definePosition(position, top, bottom) {
+  let result = null;
+
+  switch (position) {
+    case POSITIONS.TOP:
+    case POSITIONS.TOP_RIGHT:
+    case POSITIONS.TOP_LEFT:
+      result = top;
+      break;
+
+    case POSITIONS.BOTTOM:
+    case POSITIONS.BOTTOM_RIGHT:
+    case POSITIONS.BOTTOM_LEFT:
+      result = bottom;
+      break;
+  }
+
+  return result;
+}
+
+export default {
+  name: "Toaster",
+
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: "default"
+    },
+    position: {
+      type: String,
+      default: POSITIONS.BOTTOM_RIGHT,
+      validator(value) {
+        return Object.values(POSITIONS).includes(value);
+      }
+    },
+    maxToasts: {
+      type: [Number, Boolean],
+      default: false
+    },
+    duration: {
+      type: [Number, Boolean],
+      default: 4000
+    },
+    dismissible: {
+      type: Boolean,
+      default: true
+    },
+    queue: {
+      type: Boolean,
+      default: false
+    },
+    pauseOnHover: {
+      type: Boolean,
+      default: true
+    },
+    useDefaultCss: {
+      type: Boolean,
+      default: true
+    },
+    onClose: {
+      type: Function,
+      default: () => {}
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    }
+  },
+
+  setup(props) {
+    const toaster = ref(null);
+    const isActive = ref(false);
+    const data = reactive({
+      parentTop: null,
+      parentBottom: null,
+      isHovered: false,
+      queueTimer: null,
+      timer: null
+    });
+
+    function createParents() {
+      data.parentTop = document.querySelector(".toast-container-top");
+      data.parentBottom = document.querySelector(".toast-container-bottom");
+
+      if (data.parentTop && data.parentBottom) return;
+
+      if (!data.parentTop) {
+        data.parentTop = document.createElement("div");
+        data.parentTop.className = "toast-container toast-container-top";
+      }
+
+      if (!data.parentBottom) {
+        data.parentBottom = document.createElement("div");
+        data.parentBottom.className = "toast-container toast-container-bottom";
+      }
+    }
+
+    function setDefaultCss() {
+      const type = props.useDefaultCss ? "add" : "remove";
+      data.parentTop.classList[type]("default-theme");
+      data.parentBottom.classList[type]("default-theme");
+    }
+
+    function setupContainer() {
+      const container = document.body;
+      container.appendChild(data.parentTop);
+      container.appendChild(data.parentBottom);
+    }
+
+    const correctParent = computed(() =>
+      definePosition(props.position, data.parentTop, data.parentBottom)
+    );
+
+    function shouldQueue() {
+      if (!props.queue && props.maxToasts === false) return false;
+
+      if (props.maxToasts !== false) {
+        return (
+          props.maxToasts <=
+          data.parentTop.childElementCount + data.parentBottom.childElementCount
+        );
+      }
+
+      return (
+        data.parentTop.childElementCount > 0 ||
+        data.parentBottom.childElementCount > 0
+      );
+    }
+
+    function showNotice() {
+      if (shouldQueue()) {
+        data.queueTimer = setTimeout(showNotice, 250);
+        return;
+      }
+
+      correctParent.value.insertAdjacentElement("afterbegin", toaster.value);
+      isActive.value = true;
+
+      data.timer =
+        props.duration !== false ? useTimer(close, props.duration) : null;
+    }
+
+    function click() {
+      props.onClick.apply(null, arguments);
+
+      if (props.dismissible) {
+        close();
+      }
+    }
+
+    function toggleTimer(newVal) {
+      if (data.timer && props.pauseOnHover) {
+        newVal ? data.timer.pause() : data.timer.resume();
+      }
+    }
+
+    function stopTimer() {
+      data.timer && data.timer.stop();
+      clearTimeout(data.queueTimer);
+    }
+
+    function close() {
+      stopTimer();
+      isActive.value = false;
+      setTimeout(() => {
+        props.onClose.apply(null, arguments);
+        const { umount } = useComponent();
+        umount(toaster.value);
+      }, 150);
+    }
+
+    onBeforeMount(() => {
+      createParents();
+      setDefaultCss();
+      setupContainer();
+    });
+
+    onBeforeUnmount(() => {
+      eventBus.off("clear-toaster", close);
+    });
+
+    onMounted(() => {
+      showNotice();
+      eventBus.on("clear-toaster", close);
+    });
+
+    const transition = computed(() =>
+      definePosition(
+        props.position,
+        {
+          enter: "fadeInDown",
+          leave: "fadeOut"
+        },
+        {
+          enter: "fadeInUp",
+          leave: "fadeOut"
+        }
+      )
+    );
+
+    return {
+      toaster,
+      isActive,
+      click,
+      toggleTimer,
+      transition
+    };
+  }
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import "./styles.css";
+import eventBus from "./composables/event-bus";
+import useComponent from "./composables/component";
+import Toaster, { POSITIONS, definePosition } from "./Toaster";
+
+Toaster.install = (app, options = {}) => {
+  const globalOptions = options;
+  const methods = {
+    show(message, options = {}) {
+      let localOptions = { message, ...options };
+      const { mount } = useComponent();
+      const component = mount(Toaster, {
+        props: { ...globalOptions, ...localOptions }
+      });
+
+      return component;
+    },
+
+    clear() {
+      eventBus.emit("clear-toaster");
+    },
+
+    success(message, options = {}) {
+      options.type = "success";
+      return this.show(message, options);
+    },
+
+    error(message, options = {}) {
+      options.type = "error";
+      return this.show(message, options);
+    },
+
+    info(message, options = {}) {
+      options.type = "info";
+      return this.show(message, options);
+    },
+
+    warning(message, options = {}) {
+      options.type = "warning";
+      return this.show(message, options);
+    }
+  };
+
+  app.$toast = methods;
+  app.config.globalProperties.$toast = methods;
+  app.provide("toast", methods);
+};
+
+export default Toaster;
+export { Toaster, POSITIONS, definePosition };
